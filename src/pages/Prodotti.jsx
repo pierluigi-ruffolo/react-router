4906f7e7a6e1d0f6c99fd3b8c59c24982130b9e0
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-09 15:02:17 +0100

    feat: implemento  visualizzazione a griglia per le card dei prodotti

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -6,13 +6,37 @@ export default function Prodotti() {
   useEffect(() => {
     axios
       .get(" https://fakestoreapi.com/products")
-      .then((response) => console.log(response.data));
+      .then((response) => setProdotti(response.data));
   }, []);
 
   return (
     <div className="container">
       <div className="row">
-        <div className="col">
+        {console.log(prodotti)}
+        {prodotti.map((prodotto) => {
+          return (
+            <div className="col-6 mb-5 p-3" key={prodotto.id}>
+              <div className="card h-100 bg-secondary-subtle">
+                <div className="p-5 text-center">
+                  <img src={prodotto.image} alt={prodotto.title} />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title fs-2">{prodotto.title}</h5>
+                  <p className="card-text fs-4">{prodotto.category}</p>
+                  <p className="card-text fs-4">{prodotto.description}</p>
+                  <p className="card-text fs-3">Prezzo: {prodotto.price}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+{
+  /*   <div className="col">
           <div className="card">
             <div className="card">
               <img src="..." className="card-img-top" alt="..." />
@@ -24,8 +48,5 @@ export default function Prodotti() {
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  );
+        </div> */
 }
