3fe8d489f2b833ef136913b0ecc19831b8d683d8
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-09 13:54:08 +0100

    implemento header

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ChiSiamo from "./pages/ChiSiamo";
 import Prodotti from "./pages/Prodotti";
-
+import DefaultLayout from "./layouts/DefaultLayout";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ChiSiamo" element={<ChiSiamo />} />
-        <Route path="/Prodotti" element={<Prodotti />} />
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/ChiSiamo" element={<ChiSiamo />} />
+          <Route path="/Prodotti" element={<Prodotti />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
