22ebf4ed718de24a83bba4f9e8f709f9088aa262
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-09 15:46:45 +0100

    feat: modifico home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="home d-flex justify-content-center align-items-center">
-      <h1>Benvenuto nel nostro Store</h1>
+      <h1 className="text-white">Benvenuto nel nostro Store....</h1>
     </div>
   );
 }
